01d92fba94810249b762dda5178213b2506cf745
Frank (buzz) <fortgnox@mockenhaupt.org>
2021-01-17 02:13:03 +0100

    Partial rollback of commit f72343a8660e419da643d19b6127ce057f2e42c8.

--- src/main/java/org/mockenhaupt/fortgnox/MainFrame.java
+++ src/main/java/org/mockenhaupt/fortgnox/MainFrame.java
@@ -1475,6 +1475,7 @@ public class MainFrame extends JFrame implements
         jToolBarMainFunctions.add(jButtonSettings);
 
 //        jToolBarMainFunctions.add(buttonOptions);
+        jToolBarMainFunctions.add(buttonExit);
 
         jButtonSettings.setIcon(getIcon("/org/mockenhaupt/fortgnox/sprocket48.png"));
 //        jButtonSettings.setText("Settings");
@@ -1503,16 +1504,11 @@ public class MainFrame extends JFrame implements
             }
         });
 
-
-
         // Separate toolbar for about dialog (ensures correct geometry handling)
-//        JToolBar aboutPanel = new JToolBar();
-//        aboutPanel.setFloatable(false);
-//        aboutPanel.add(buttonAbout);
-        jToolBarMainFunctions.add(new JSeparator());
-        jToolBarMainFunctions.add(buttonExit);
-        jToolBarMainFunctions.add(buttonAbout);
-
+        JToolBar aboutPanel = new JToolBar();
+        aboutPanel.setFloatable(false);
+        aboutPanel.add(buttonAbout);
+        jToolBarPanel.add(aboutPanel, BorderLayout.LINE_END);
 
         jToolBarPanel.add(jToolBarMainFunctions, BorderLayout.CENTER);
         getContentPane().add(jToolBarPanel, java.awt.BorderLayout.PAGE_START);
