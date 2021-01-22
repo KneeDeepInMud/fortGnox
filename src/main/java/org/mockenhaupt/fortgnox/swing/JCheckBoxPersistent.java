0fd88819f4239bd5d4aab13efa7f2e73656e782a
Frank (buzz) <fortgnox@mockenhaupt.org>
2021-01-22 23:15:03 +0100

    Introduced persistent checkbox in option panel

--- src/main/java/org/mockenhaupt/fortgnox/swing/JCheckBoxPersistent.java
+++ src/main/java/org/mockenhaupt/fortgnox/swing/JCheckBoxPersistent.java
@@ -6,13 +6,25 @@ import javax.swing.JCheckBox;
 
 public class JCheckBoxPersistent extends JCheckBox
 {
+    public JCheckBoxPersistent (String preference)
+    {
+        this(preference, null, null);
+    }
+    public JCheckBoxPersistent (String preference, String text)
+    {
+        this(preference, text, null);
+    }
+
     public JCheckBoxPersistent (String preference, String text, Runnable onAction)
     {
         super(text);
         this.setSelected(FgPreferences.get().getBoolean(preference));
         addActionListener(actionEvent -> {
             FgPreferences.get().putPreference(preference, JCheckBoxPersistent.this.isSelected());
-            onAction.run();
+            if (onAction != null)
+            {
+                onAction.run();
+            }
         });
     }
 }
